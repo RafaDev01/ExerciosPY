5e144baefdcd4a27c5c06f3e91bb95f113c9c9bc
Rafael Nascimento <88472729+RafaDev01@users.noreply.github.com>
2022-04-08 10:20:36 -0300

    commit

---
+++ ex006.py
@@ -0,0 +1,4 @@
+metro = float(input('Digite um número em metros: '))
+cm = metro * 100
+ml = metro * 1000
+print('{} metro(s). convertido em cm é igual a: {} \n e convertido em milímetros é igual a : {}'.format(metro, cm, ml))
