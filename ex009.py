c437fd36e514ebc70108f28db97d8bca6c7f3c92
Rafael Nascimento <88472729+RafaDev01@users.noreply.github.com>
2022-04-09 10:29:22 -0300

    commit

---
+++ ex009.py
@@ -0,0 +1,5 @@
+a = float(input('Digite a altura da parede: '))
+l = float(input('Digite a largura da parede: '))
+mquadrado = a * l
+ltinta = mquadrado / 2
+print('A parede possui {} m² \n Você vai precisar de {} litro(s) de tinta'.format(mquadrado, ltinta))
