aa175ef11c84e8bc09de8d9e4059c3d09ac50121
Rafael Nascimento <88472729+RafaDev01@users.noreply.github.com>
2022-04-27 00:38:49 -0300

    commit

--- ex003.py
+++ ex003.py
@@ -2,3 +2,5 @@ n = int(input('Digite um número: '))
 a = n - 1
 s = n + 1
 print('O sucessor de {} é {} e o antecessor é {}.'.format(n, s, a))
+#Poderia ser feito também sem as variaveis
+print('O sucessor de {} é {} e o antecessor é {}.'.format(n, (n+1), (n-1)))
--- ex004.py
+++ ex004.py
@@ -2,5 +2,9 @@ n = int(input('Digite um número: '))
 d = n * 2
 t = n * 3
 r = n ** (1/2)
-print('Número digitado: {} \n dobro: {} \n triplo: {} \n raiz quadrada {}.'.format(n, d, t, r))
-
+print('Número digitado: {} \n dobro: {} \n triplo: {} \n raiz quadrada {:.2f}.'.format(n, d, t, r))
+#{:.2f} representa -> 2 casas decimais flutuantes
+#poderia ser feito tambem:
+print('Número digitado: {} \n dobro: {} \n triplo: {} \n raiz quadrada {:.2f}.'.format(n, (n*2), (n*3), (n**(1/2))))
+print('-----------------------------\n------------------------')
+print('Número digitado: {} \n dobro: {} \n triplo: {} \n raiz quadrada {:.2f}.'.format(n, (n*2), (n*3), pow(n, (1/2))))
